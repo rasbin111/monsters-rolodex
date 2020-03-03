009b3488127f7adf667779369b375951c7d833ec
rasbin <rasbin@perplexsolutions.com>
2020-03-03 15:13:36 +0545

    Updates

---
+++ precache-manifest.001b0dbec3eecb7ebda55d97122126db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3503afc0173779f2dee5542d06b195",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bfde26af71e322c1a743",
+    "url": "/monsters-rolodex/static/css/main.70878f7c.chunk.css"
+  },
+  {
+    "revision": "c90f681afa1a0dc0b31c",
+    "url": "/monsters-rolodex/static/js/2.7078fee5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.7078fee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfde26af71e322c1a743",
+    "url": "/monsters-rolodex/static/js/main.66d2b51d.chunk.js"
+  },
+  {
+    "revision": "1672be7e2e42305550d3",
+    "url": "/monsters-rolodex/static/js/runtime-main.8a2e9529.js"
+  }
+]);
